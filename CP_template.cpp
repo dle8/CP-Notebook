76dfd1d57636e300392e4b882ba3f95e7f37f5dd
dle8 <dle8@u.rochester.edu>
2018-10-16 15:24:27 -0400

    Update cp template.

--- CP_template.cpp
+++ CP_template.cpp
@@ -4,6 +4,7 @@ University of Rochester
 */
 
 #include <bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(false); cin.tie(NULL)
 #define FOR(i, l, r) for (int i=l; i<=r; i++)
 #define REP(i, r, l) for (int i=r; i>=l; i--)
 #define mp make_pair
@@ -23,17 +24,23 @@ typedef pair<int, int> pii;
 typedef pair<long, long> pll;
 typedef pair<ll, ll> pllll;
 typedef pair<ld, ld> Point;
-typedef pair<Point, Point> Line;
+typedef pair<Point, Point> line;
 struct strLine { ld a, b, c; };
 
 typedef vector<int> vi;
 typedef vector<long> vl;
 typedef vector<ll> vll;
-typedef vector<ld> vld;
+
+const double pi = 3.141592653589793;
+
+ll gcd(ll a, ll b) { return (b==0)?a:gcd(b, a%b); } 
+ll lcm(ll a, ll b) { return (a*b)/gcd(a, b); }
+ll max(ll a, ll b) { return (a>=b)?a:b; }
+ll min(ll a, ll b) { return (a<=b)?a:b; }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	
+	fio;
+		
 //	freopen(".in", "r", stdin);
 //	freopen(".out", "w", stdout);	
 	
