e662a22be86f9c63f3bd95d90b5813f1cd69567b
dle8 <dle8@u.rochester.edu>
2018-10-24 01:02:12 -0400

    Add FordBellman algo for finding shortest path

---
+++ Graphs/FordBellman.cpp
@@ -0,0 +1,16 @@
+/*
+  Author: Dung Tuan Le
+  University of Rochester
+*/
+
+/*
+  Ford-Bellman algorithm: find the shortest path between 2 vertices (u, v) in a graph that
+  contains negative-valued edges, but does not contains negative circuit
+*/
+
+#define oo 1e9
+
+long FordBellman(long u, long) {
+  FOR(i, 1, n) d[i] = oo;
+  d[i]
+}
