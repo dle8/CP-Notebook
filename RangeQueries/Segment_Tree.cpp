ebc28c21b476955fe62a612affb1d13647e914d2
dle8 <dle8@u.rochester.edu>
2018-10-23 21:27:42 -0400

    Fix minor issues in comments

--- RangeQueries/Segment_Tree.cpp
+++ RangeQueries/Segment_Tree.cpp
@@ -5,26 +5,26 @@
 */
 
 /*
-  A segment tree is data structure that provides 2 operations (both run in O(logn) time complexsty):
+  A Segment Tree is data structure that provides 2 operations (both run in O(logn) time complexsty):
   - Process a range query: eg. find the number that contains a specific special features in a range(min, max in value)
                                   or sum of all elements' value in the given range.
   - Update an array value: (have to update for at most O(logn) other nodes in the tree)
 */
 
 /*
-  Below is the example of segment tree that returns a minimum value in a given range
+  Below is the example of Segment Tree that returns a minimum value in a given range
 */
 #define oo 1e9
 #define maxn 1000+10
 long st[3 * maxn];
 
-// ind is the index of the node in the segment tree
+// ind is the index of the node in the Segment Tree
 
 long min(long a, long b) { return (a <= b) ? a : b; }
 
 // init all values of st = oo;
 
-// update whole values of array a indexed from l to r into segment tree
+// update whole values of array a indexed from l to r into Segment Tree
 void update(long ind, long l, long r) {
   if (l == r) {
     st[id] = a[l];
@@ -36,7 +36,7 @@ void update(long ind, long l, long r) {
   st[id] = min(st[2 * id], st[2 * id + 1]);
 }
 
-// update a[i] whose value is val into segment tree
+// update a[i] whose value is val into Segment Tree
 void update(long ind, long l, long r, long i, long val) {
   if (i < l || r < i) return;
   st[cs] = min(st[cs], val);
