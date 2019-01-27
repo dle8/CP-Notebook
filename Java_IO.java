fe35765a07805259114fcc19ce2edf5259b80751
dle8 <dle8@u.rochester.edu>
2019-01-26 19:27:34 -0500

    Add read input & write output from file template

---
+++ Java_IO.java
@@ -0,0 +1,20 @@
+import java.util.*;
+// import java.lang.*;
+import java.lang.Math;
+import java.io.*;
+import java.math.BigInteger;
+
+public class test {
+
+	public static void main (String[] args) throws java.lang.Exception {
+		File filein = new File("input.txt");
+		File fileout = new File("output.txt");
+		Scanner in = new Scanner(filein);
+		PrintWriter out = new PrintWriter(fileout);
+
+		// input is similar to normal Scanner
+
+		// out.println("");
+		out.close();
+	}
+}
