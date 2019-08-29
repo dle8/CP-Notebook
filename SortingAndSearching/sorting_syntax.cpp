01e798939f086c836492f20f4b623f9b4ce2287d
dle8 <dle8@u.rochester.edu>
2019-08-29 12:26:20 +0700

    Ways to customize sorting methods in C++

---
+++ SortingAndSearching/sorting_syntax.cpp
@@ -0,0 +1,14 @@
+struct point {
+  int x, y;
+  bool operator<(const point &p) {
+    if (x == p.x) return y < p.y;
+    else return x < p.x;
+  }
+};
+
+bool comp(string a, string b) {
+  if (a.size() == b.size()) return a < b;
+  else return a.size() < b.size();
+}
+
+// sort(v.begin(), v.end(), cmp);
