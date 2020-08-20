3b1ae048da713b6f110ed4ef1535dcaca27b9f11
Dylan Le <dle8@u.rochester.edu>
2020-08-20 16:14:38 -0700

    Add notes to explain ford bellman

--- Graphs/FordBellman.cpp
+++ Graphs/FordBellman.cpp
@@ -24,6 +24,9 @@ long FordBellman(long s, long t) {
   FOR(i, 1, n) d[i] = oo;
   d[s] = 0;
 
+  // Relax all E edges V - 1 times.
+  // If E edges are relaxed V - 1 times, the shortest path from the source vertex to furthest vertex from the source
+  // (which will be a simple path with V - 1 edges) should have been correctly computed.
   FOR(i, 1, n - 1) {
     for (auto edge: edges) {
         long u, v, c;
